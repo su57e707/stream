d43886cbf598edca43bd583ab45e81d06019ad82
su57e707 <hem23255@gmail.com>
2026-01-03 00:51:49 +0300

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,7 +1,9 @@
 { pkgs }: {
   deps = [
-    pkgs.google-chrome-stable
+    # This is the correct name for Chromium (the open-source version of Chrome) in the Replit Nix environment.
+    pkgs.chromium
+
+    # This is the correct name for the virtual screen package.
     pkgs.xvfb_run
-    pkgs.xorg.libxshmfence
   ];
 }
